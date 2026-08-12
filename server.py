80426b0105fee6bfb101c8ae46f2565437c583a1
munnagambler <mudasirkhan5820@gmail.com>
2026-08-12 10:18:11 +0530

    Change server port from 7860 to 8000

--- server.py
+++ server.py
@@ -1562,4 +1562,4 @@ if __name__ == "__main__":
     except Exception:
         pass
 
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 7860)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
